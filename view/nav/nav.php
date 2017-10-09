dd8ddee7962ce48844a13a2e8279f162922eb315
Allen Goodreds <allen@goodreds.net>
2017-10-09 21:16:14 +1000

    Navigation updated
    - nav.php: uploaded correct version

--- view/nav/nav.php
+++ view/nav/nav.php
@@ -8,67 +8,66 @@
  * Putro, Edwan - edwanhp@gmail.com
  */
 ?>
-    <nav class="navbar navbar-default navbar-inverse">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed"
-                        data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
-                        aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span> <span
-                            class="icon-bar"></span> <span class="icon-bar"></span> <span
-                            class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand hidden-xs" href="<?php echo BASE; ?>/Home">Humphree</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse"
-                 id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-									<?php
-									if (isset ( $_SESSION [MODULE] )) {
-										echo '<li><a href="' . BASE . '/Dashboard">Dashboard</a></li>';
-									}
-									?>
-
-									<?php
-									if (isset ( $_SESSION ['status'] )) {
-										if ($_SESSION ['status'] == 'admin') {
-											echo '<li><a href="' . BASE . '/Administration">Admin</a></li>';
-										}
-									}
-									?>
-                </ul>
-                <!--  Note:  To obtain easy access to CreateDB, uncomment this link while testing to reset and reseed the database. -->
-                <ul class="nav navbar-nav navbar-right">
-									<?php
-									if (isset ( $_SESSION [MODULE] )) {
-										echo '<li><a href="' . BASE . '/CreateDB">Create DB</a></li>';
-									} else {
-										// Remove this on the Production Server
-										echo '<li><a href="' . BASE . '/CreateDB">Create DB</a></li>';
-									}
-									?>
-                </ul>
-							<?php
-							if (isset ( $_SESSION [MODULE] )) {
-								echo '<ul class="nav navbar-nav navbar-right">
-		<li><a href="' . BASE . '/ChangePassword">Change Password</a></li>
-		<li><a href="' . BASE . '/Logout">Logout</a></li>
-      </ul>';
-							} else {
-								echo '<ul class="nav navbar-nav navbar-right">
-		<li><a href="' . BASE . '/Signup">Sign-up</a></li>
-		<li><a href="' . BASE . '/Signin">Sign-in</a></li>
-      </ul>';
-							}
-							?>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
-
-<?php
-?>
+<!--Nav-->
+<div class="container-fluid">
+  <div class="row">
+    <!-- Left margin -->
+    <div class="col"></div>
+    <!-- Central column -->
+    <div class="col-9">
+      <ul class="nav nav-pills nav-justified top-n-tail">
+        <li class="nav-item">
+          <a class="nav-link active" href="<?php echo BASE; ?>/Home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">View listings</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">My account</a>
+          <div class="dropdown-menu">
+            <?php
+            if (isset ( $_SESSION [MODULE] )) {
+                echo '<a class="dropdown-item" href="' . BASE . '/Dashboard">Dashboard</a>';
+              }
+            ?>
+            <?php
+            if (isset ( $_SESSION ['status'] )) {
+                if ($_SESSION ['status'] == 'admin') {
+                echo '<a class="dropdown-item" href="' . BASE . '/Administration">Admin</a>';
+              }
+            }
+            ?>
+            <?php
+            if (isset ( $_SESSION [MODULE] )) {
+                echo '<div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="' . BASE . '/ChangePassword">Change Password</a>
+                      <a class="dropdown-item" href="' . BASE . '/Logout">Logout</a>';
+            } else {
+                echo '<div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="' . BASE . '/Signup">Sign-up</a>
+                      <a class="dropdown-item" href="' . BASE . '/Signin">Sign-in</a>';
+                  }
+            ?>
+            <?php
+            if (isset ( $_SESSION [MODULE] )) {
+                echo '<div class="dropdown-divider"></div>
+                      <a class="dropdown-item disabled" href="' . BASE . '/CreateDB">Create DB</a>';
+            } else {
+                // Remove this on the Production Server
+                echo '<div class="dropdown-divider"></div>
+                      <a class="dropdown-item disabled" href="' . BASE . '/CreateDB">Create DB</a>';
+            }
+            ?>
+            <a class="dropdown-item disabled" href="#">Test</a>
+            <a class="dropdown-item disabled" href="#">Create DB</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Logout</a>
+            <!--<a class="dropdown-item" href="#">Separated link</a>-->
+          </div>
+        </li>
+      </ul>
+    </div> <!-- end col-9 -->
+    <!-- Right margin -->
+    <div class="col"></div>
+  </div> <!-- end row -->
+</div> <!-- end container -->
