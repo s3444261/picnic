58a73bf944ee3c7d0fb6e629feae6b998fc9c2d3
Allen Goodreds <allen@goodreds.net>
2017-10-10 21:45:34 +1000

    Added favicon

--- view/header/header.php
+++ view/header/header.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Authors: 
+ * Authors:
  * Derrick, Troy - s3202752@student.rmit.edu.au
  * Foster, Diane - s3387562@student.rmit.edu.au
  * Goodreds, Allen - s3492264@student.rmit.edu.au
@@ -37,9 +37,11 @@
 			      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 			    <![endif]-->
+    <link rel="shortcut icon" href="<?php echo BASE; ?>/img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?php echo BASE; ?>/img/favicon.ico" type="image/x-icon">
 </head>
 <body>
-<?php 
+<?php
 
 if(isset($_SESSION['error'])){
 	echo '<span class="error">' . $_SESSION['error'] . '</span>';
