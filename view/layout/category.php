14b551444093b05c1348c8961e7238b5702d2b46
Allen Goodreds <allen@goodreds.net>
2017-10-13 10:16:57 +1000

    Corrected markup of category.php

--- view/layout/category.php
+++ view/layout/category.php
@@ -55,51 +55,58 @@
     <!-- Central column -->
     <div class="col-9">
       <div class="container">
-      <ul class="nav nav-tabs nav-justified">
-        <li class="nav-item active">
-          <a class="nav-link" href="#1" data-toggle="tab">For sale</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#2" data-toggle="tab">Wanted</a>
-        </li>
-      </ul>
-      <div class="tab-content ">
-        <div class="tab-pane active" id="1">
-          <div class="row">
-            <?php foreach ( $this->data['forSaleItems'] as $item ) { ?>
+        <div class="row">
+          <div class="col"></div> <!-- left margin -->
+          <div class="col-md-9">
+            <ul class="nav nav-tabs nav-justified">
+              <li class="nav-item active">
+                <a class="nav-link" href="#1" data-toggle="tab">For sale</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#2" data-toggle="tab">Wanted</a>
+              </li>
+            </ul>
+            <div class="tab-content ">
+              <div class="tab-pane active" id="1">
+                <div class="row">
+                  <?php foreach ( $this->data['forSaleItems'] as $item ) { ?>
 
-              <div class="col-sm-6 col-md-4 col-xl-3 top-n-tail">
-                <div class="card">
-                  <img class="card-img-top" src="<?php echo BASE.'/Item/Thumb/'.$item['itemID'] ?>" alt="Card image cap">
-                  <div class="card-body">
-                    <h4 class="card-title"><?php echo $item['title'] ?></h4>
-                    <p class="card-text">Short description of item here. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
-                    <a href="<?php echo BASE.'/Item/View/'.$item['itemID'] ?>" class="btn btn-primary btn-block">Info</a>
-                  </div>
+                    <div class="col-sm-6 col-md-4 col-xl-3 top-n-tail">
+                      <div class="card">
+                        <img class="card-img-top" src="<?php echo BASE.'/Item/Thumb/'.$item['itemID'] ?>" alt="Card image cap">
+                        <div class="card-body">
+                          <h4 class="card-title"><?php echo $item['title'] ?></h4>
+                          <p class="card-text">Short description of item here. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
+                          <a href="<?php echo BASE.'/Item/View/'.$item['itemID'] ?>" class="btn btn-primary btn-block">Info</a>
+                        </div>
+                      </div>
+                    </div>
+                  <?php } ?>
                 </div>
-              </div>
-            <?php } ?>
-          </div>
-        </div> <!-- end of tab pane #1 -->
+              </div> <!-- end of tab pane #1 -->
 
-        <div class="tab-pane active" id="2">
-          <div class="row">
-            <?php foreach ( $this->data['wantedItems'] as $item ) { ?>
+              <div class="tab-pane active" id="2">
+                <div class="row">
+                  <?php foreach ( $this->data['wantedItems'] as $item ) { ?>
 
-              <div class="col-sm-6 col-md-4 col-xl-3 top-n-tail">
-                <div class="card">
-                  <img class="card-img-top" src="<?php echo BASE.'/Item/Thumb/'.$item['itemID'] ?>" alt="Card image cap">
-                  <div class="card-body">
-                    <h4 class="card-title"><?php echo $item['title'] ?></h4>
-                    <p class="card-text">Short description of item here. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
-                    <a href="<?php echo BASE.'/Item/View/'.$item['itemID'] ?>" class="btn btn-primary btn-block">Info</a>
-                  </div>
+                    <div class="col-sm-6 col-md-4 col-xl-3 top-n-tail">
+                      <div class="card">
+                        <img class="card-img-top" src="<?php echo BASE.'/Item/Thumb/'.$item['itemID'] ?>" alt="Card image cap">
+                        <div class="card-body">
+                          <h4 class="card-title"><?php echo $item['title'] ?></h4>
+                          <p class="card-text">Short description of item here. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
+                          <a href="<?php echo BASE.'/Item/View/'.$item['itemID'] ?>" class="btn btn-primary btn-block">Info</a>
+                        </div>
+                      </div>
+                    </div>
+                  <?php } ?>
                 </div>
-              </div>
-            <?php } ?>
+              </div> <!-- end of tab pane #2 -->
+            </div> <!-- end of .tab-content -->
           </div>
-        </div> <!-- end of tab pane #2 -->
-      </div> <!-- end of .tab-content -->
+          <div class="col"></div> <!-- right margin -->
+        </div>
+
     </div> <!-- end .container -->
   </div> <!-- end .col-9 -->
   <!-- Right margin -->
