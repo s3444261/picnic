d0dbef7a39e15d054a7a1c4e1768b875cc01a5c1
Allen Goodreds <allen@goodreds.net>
2017-10-13 10:52:52 +1000

    Removed button styling from category links
    class="btn btn-secondary" changed to class=""

--- view/layout/category.php
+++ view/layout/category.php
@@ -27,7 +27,7 @@
         <div class="container">
     			<?php foreach ( $this->data['subCategories'] as $category ) { ?>
             <div class="col-md-4">
-              <a class="btn btn-secondary" href="<?php echo BASE.'/Category/View/'.$category['categoryID'] ?>" role="button"><?php echo $category['category'] ?></a>
+              <a class="" href="<?php echo BASE.'/Category/View/'.$category['categoryID'] ?>" role="button"><?php echo $category['category'] ?></a>
             </div>
     			<?php } ?>
         </div>
