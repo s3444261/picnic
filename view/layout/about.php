fa0d4c3bdfff384687ea09dcd69f60d285d9ccfe
Troy Derrick <s3202752@student.rmit.edu.au>
2017-10-14 17:39:15 +1100

    Put some words in the About page.

--- view/layout/about.php
+++ view/layout/about.php
@@ -8,12 +8,27 @@
  * Putro, Edwan - edwanhp@gmail.com
  */
 ?>
+
 <div class="container-fluid">
-    <h1>Humphree</h1>
-    <div class="jumbotron">
-        <div class="row">
-            <h2>About Humphree</h2>
+    <div class="row">
+        <div class="col"></div> <!-- left margin -->
+        <div class="col-md-9 text-center">
+            <h1 class="text-center">Humphree</h1>
+            <h2>About Us</h2>
+            <p>Humphree is a place where people come to buy and sell their stuff.</p>
+            <p>We match buyers and sellers, to make it easy to find what you need, and to get rid of what you don't.</p>
+            <p>Established in 2017, we are one of the newest market places on the web - what sets us apart are our sophisticated matching algorithms that find you what you want, when you want it. To find an item, simply post a "Wanted" listing and wait for the matches to start rolling in. To sell, post a "For Sale" listing and sit back as potential buyers are notified of what you have.</p>
+            <p>Once a match is made, buyer and seller can contact each other via Humphree, and agree on a time and place to make the sale. Meet up, check out the goods and if you're happy, do the deal!</p>
+            <h2>Contact Us</h2>
+            <p>
+                Humphree Pty Ltd<br/>
+                300 Collins Street<br/>
+                Melbourne VIC 3000<br/>
+            </p>
+            <p><a href="mailto:enquiries@humpree.org">enquiries@humpree.org</a></p>
+            <p> (03) 8555 2555 </p>
         </div>
+        <div class="col"></div> <!-- right margin -->
     </div>
 </div>
 
