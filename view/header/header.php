7af2c644d255db86102af1803ed400ed99ad2349
Allen Goodreds <allen@goodreds.net>
2017-10-14 12:51:10 +1000

    Tidied-up spacing in header.php

--- view/header/header.php
+++ view/header/header.php
@@ -20,25 +20,24 @@
 <script src="modernizr.js"></script>
 <meta name="robots" content="noindex, nofollow" />
 <title>Picnic</title>
+<!-- Bootstrap via CDN -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<!-- Material icons CSS -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
 
-    <!-- Bootstrap via CDN -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <!-- Material icons CSS -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
-
-    <!-- Local Bootstrap-->
-    <link href="<?php echo BASE; ?>/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo BASE; ?>/css/main.css" rel="stylesheet">
+<!-- Local Bootstrap-->
+<link href="<?php echo BASE; ?>/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo BASE; ?>/css/main.css" rel="stylesheet">
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
-			      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-			      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-			    <![endif]-->
-    <link rel="shortcut icon" href="<?php echo BASE; ?>/img/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="<?php echo BASE; ?>/img/favicon.ico" type="image/x-icon">
+		      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		    <![endif]-->
+<link rel="shortcut icon" href="<?php echo BASE; ?>/img/favicon.ico" type="image/x-icon">
+<link rel="icon" href="<?php echo BASE; ?>/img/favicon.ico" type="image/x-icon">
 </head>
 <body>
 <?php
@@ -70,11 +69,11 @@ if(isset($_SESSION['message'])){
                         <button class="btn btn-default" type="submit">
                             <i class="material-icons md-light">search</i>
                         </button>
-                    </div>
-                </div>
+                    </div> <!-- end .input-group-btn -->
+                </div> <!-- end .input-group .add-on -->
             </form>
         </div> <!-- end col-9 -->
         <!-- Right margin -->
         <div class="col"></div>
-    </div> <!-- end row -->
-</div> <!-- end container -->
+    </div> <!-- end .row -->
+</div> <!-- end .container-fluid -->
