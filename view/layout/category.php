8e27db66dc8fc30929926cc1fea34b1cee69de74
Allen Goodreds <allen@goodreds.net>
2017-10-15 12:06:49 +1000

    Styled truncated header and description text
    Updated truncate style rules to account for different length requirement for header and description.

--- view/layout/category.php
+++ view/layout/category.php
@@ -71,11 +71,10 @@
                 <div class="card">
                   <img class="card-img-top" src="<?php echo BASE.'/Item/Thumb/'.$item['itemID'] ?>" alt="Card image cap">
                   <div class="card-body">
-                    <h4 class="card-title"><?php echo $item['title'] ?></h4>
+                    <h4 class="card-title truncate"><?php echo $item['title'] ?></h4>
                     <p class="card-text"><?php echo '$' . $item['price'] ?></p>
-                    <p class="card-text truncate">Short description of item here. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
-                    <p class="card-text">Short description of item here. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
-                    <p class="card-text"><?php echo '$' . $item['price'] ?></p>
+                    <p class="card-text truncate_paragraph">Short description of item here. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
+                    <h4 class="card-text text-center price"><?php echo '$' . $item['price'] ?></h4>
                     <a href="<?php echo BASE.'/Item/View/'.$item['itemID'] ?>" class="btn btn-primary btn-block">Info</a>
                   </div>
                 </div>
