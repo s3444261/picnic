2313a1907e5a8c0ad6c5f6f45c782c806853d697
Allen Goodreds <allen@goodreds.net>
2017-10-15 22:36:28 +1000

    Changed markup to link favicon.ico in root folder

--- view/header/header.php
+++ view/header/header.php
@@ -36,8 +36,8 @@
 		      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		    <![endif]-->
-<link rel="shortcut icon" href="<?php echo BASE; ?>/img/favicon.ico" type="image/x-icon">
-<link rel="icon" href="<?php echo BASE; ?>/img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="<?php echo BASE; ?>/favicon.ico" type="image/x-icon">
+<link rel="icon" href="<?php echo BASE; ?>/favicon.ico" type="image/x-icon">
 </head>
 <body>
 <?php
