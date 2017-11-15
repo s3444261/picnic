0c0141fc3f912e4bbc12c5fd50b57d1867ffd1c0
Troy Derrick <s3202752@student.rmit.edu.au>
2017-11-15 11:53:30 +1100

    Use high res imnages everywhere, for now.

--- controller/ItemController.php
+++ controller/ItemController.php
@@ -217,31 +217,34 @@ class ItemController {
 	 */
 	public function Thumb(int $itemId) {
 
-		// to avoid killing the file system with millions of files in a single directory,
-		// we store images in a structure like so:
-		//
-		// THUMB_DIRECTORY/XXX/YYYYYYY.jpg
-		//
-		// ... where XXX is the number of thousands in the item ID, and YYYYYY is the item ID itself.
-		//
-		// Examples:
-		// Item 34567 would be found at:  THUMB_DIRECTORY/34/34567.jpg
-		// Item 1234 would be found at:  THUMB_DIRECTORY/1/1234.jpg
-		// Item 8 would be found at:  THUMB_DIRECTORY/0/8.jpg
-		$paddedItemId = str_pad($itemId, 4, '0', STR_PAD_LEFT);
-		$subDir = substr($paddedItemId, 0 , strlen($paddedItemId) - 3);
-
-		$path = self::THUMB_DIRECTORY . "/" . $subDir . "/" .$itemId . ".jpg";
-
-		if (file_exists($path)) {
-			readfile($path);
-			header("Content-Type: image/jpeg");
-		} else if (file_exists(self::DEFAULT_THUMB)) {
-			readfile(self::DEFAULT_THUMB);
-			header("Content-Type: image/jpeg");
-		} else {
-			http_response_code(404);
-		}
+		$this->Image($itemId);
+		return;
+//
+//		// to avoid killing the file system with millions of files in a single directory,
+//		// we store images in a structure like so:
+//		//
+//		// THUMB_DIRECTORY/XXX/YYYYYYY.jpg
+//		//
+//		// ... where XXX is the number of thousands in the item ID, and YYYYYY is the item ID itself.
+//		//
+//		// Examples:
+//		// Item 34567 would be found at:  THUMB_DIRECTORY/34/34567.jpg
+//		// Item 1234 would be found at:  THUMB_DIRECTORY/1/1234.jpg
+//		// Item 8 would be found at:  THUMB_DIRECTORY/0/8.jpg
+//		$paddedItemId = str_pad($itemId, 4, '0', STR_PAD_LEFT);
+//		$subDir = substr($paddedItemId, 0 , strlen($paddedItemId) - 3);
+//
+//		$path = self::THUMB_DIRECTORY . "/" . $subDir . "/" .$itemId . ".jpg";
+//
+//		if (file_exists($path)) {
+//			readfile($path);
+//			header("Content-Type: image/jpeg");
+//		} else if (file_exists(self::DEFAULT_THUMB)) {
+//			readfile(self::DEFAULT_THUMB);
+//			header("Content-Type: image/jpeg");
+//		} else {
+//			http_response_code(404);
+//		}
 	}
 
 	/**
